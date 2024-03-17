00f5f06ca8d6a60f1b253845b5ca8d7a02faacdc
dadaPipes <markoliverstockflethstaf@gmail.com>
2024-03-17 13:23:37 +0100

    Initial commit

---
+++ Shared/WeatherApiClient.cs
@@ -0,0 +1,14 @@
+﻿namespace WeatherAppVSA.Shared
+{
+    public class WeatherApiClient
+    {
+        public HttpClient Client { get; }
+
+        public WeatherApiClient(HttpClient client, IConfiguration configuration)
+        {
+            var baseUrl = configuration["WeatherApi:BaseUrl"];
+            client.BaseAddress = new Uri(baseUrl!);
+            Client = client;
+        }
+    }
+}
---
+++ WeatherApiOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace WeatherAppVSA
+{
+    public class WeatherApiOptions
+    {
+        public string ApiKey { get; set; } = null!;
+        public string BaseUrl { get; set; } = string.Empty;
+    }
+}
